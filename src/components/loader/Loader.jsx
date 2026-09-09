defacb7948c16c302125543fe2f33f1ba26f921d
KASAIJA KENNETH <kasaijak@gmail.com>
2026-09-09 18:17:35 +0300

    implement loading, error and success states

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import './loader.scss';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader__spinner"></div>
+      <p>Loading products...</p>
+    </div>
+  );
+};
+
+export default Loader;
