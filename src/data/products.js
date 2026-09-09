d4375d3ca6c4ea7f13aad66b27f13c53438e02f1
KASAIJA KENNETH <kasaijak@gmail.com>
2026-09-09 15:23:34 +0300

    display product card

---
+++ src/data/products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    id: 1,
+    title: 'iPhone 9',
+    price: 549,
+    category: 'Smartphones',
+    thumbnail: 'https://dummyjson.com/image/400x300',
+  },
+
+  {
+    id: 2,
+    title: 'MacBook Pro',
+    price: 1749,
+    category: 'Laptops',
+    thumbnail: 'https://dummyjson.com/image/400x300',
+  },
+];
+
+export default products;
