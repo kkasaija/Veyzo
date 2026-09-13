9c106110020ddfd8d0fd1aa16caa247220b3ccb9
KASAIJA KENNETH <kasaijak@gmail.com>
2026-09-13 18:40:07 +0300

    implement the usePagination hook

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,18 +1,29 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 
 const usePagination = (items, itemsPerPage = 12) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(items.length / itemsPerPage);
-  const paginatedItems = useMemo(() => {
+  
+  useEffect(() => {
+    if (currentPage !== 1) {
+      setCurrentPage(1);
+    }
+  }, [items, currentPage]);
+
+  const { totalPages, visibleItems } = useMemo(() => {
+    const totalPages = Math.ceil(items.length / itemsPerPage);
+
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    return items.slice(startIndex, endIndex);
+
+    const visibleItems = items.slice(startIndex, endIndex);
+
+    return { totalPages, visibleItems };
   }, [items, currentPage, itemsPerPage]);
 
   return {
     currentPage,
     totalPages,
-    paginatedItems,
+    visibleItems,
     setCurrentPage,
   };
 };
