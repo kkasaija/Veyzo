46e2842ab2807cc570a0c270796f3fc69bfe4e23
KASAIJA KENNETH <kasaijak@gmail.com>
2026-09-13 18:54:27 +0300

    add basic page navigation

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,7 +1,42 @@
 import './pagination.scss';
 
-const Pagination = () => {
-  return <div>Pagination</div>;
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  return (
+    <nav>
+      <button
+        className="pagination__button"
+        disabled={currentPage === 1}
+        onClick={() => onPageChange(currentPage - 1)}
+      >
+        back
+      </button>
+      {Array(totalPages)
+        .fill(null)
+        .map((_, index) => {
+          const page = index + 1;
+          return (
+            <button
+              key={page}
+              onClick={() => onPageChange(page)}
+              className={
+                currentPage === page
+                  ? 'pagination__button pagination__button--active'
+                  : 'pagination__button'
+              }
+            >
+              {page}
+            </button>
+          );
+        })}
+      <button
+        className="pagination__button"
+        disabled={currentPage === totalPages}
+        onClick={() => onPageChange(currentPage + 1)}
+      >
+        next
+      </button>
+    </nav>
+  );
 };
 
 export default Pagination;
